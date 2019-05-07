6eab01b5e369b6760a68cbc80e9ce16e555cfec6
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-07 16:03:59 +0200

    ✨ Added filter collapse event

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+'use strict';
+
+(function() {
+
+  const filter = document.querySelector('#p3-filter'),
+        filterArrowDown = document.querySelector('#p3-filter i:last-of-type')
+
+  filterArrowDown.addEventListener('click', () => {
+    filter.classList.toggle('p3-filter-expanded')
+    filterArrowDown.classList.toggle('p3-arrow-turn')
+  })
+
+}())
