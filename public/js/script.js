2a44f2397d70bd8a9dca761d09dc52a75399688d
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-08 10:42:32 +0200

    ♻️ Added filterForm to .addEventListener

--- public/js/script.js
+++ public/js/script.js
@@ -3,11 +3,13 @@
 (function() {
 
   const filter = document.querySelector('#p3-filter'),
-        filterArrowDown = document.querySelector('#p3-filter i:last-of-type')
+        filterArrowDown = document.querySelector('#p3-filter i:last-of-type'),
+        filterForm = document.querySelector('#p3-filter div:last-of-type')
 
   filterArrowDown.addEventListener('click', () => {
     filter.classList.toggle('p3-filter-expanded')
     filterArrowDown.classList.toggle('p3-arrow-turn')
+    filterForm.classList.toggle('p3-filter-form-expanded')
   })
 
 }())
