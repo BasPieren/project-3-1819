f4a511072bbe825eaafe2fb76bd6ebf05f9b1fd5
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-09 12:55:01 +0200

    ✨ Added filterForm to turnFilterArrow()

--- public/js/modules/turn-filter-arrow.js
+++ public/js/modules/turn-filter-arrow.js
@@ -1,10 +1,12 @@
 function turnFilterArrow() {
   const filter = document.querySelector('#p3-filter-runs'),
-        filterArrowDown = document.querySelector('#p3-filter-runs div:first-of-type i:last-of-type')
+        filterArrowDown = document.querySelector('#p3-filter-runs div:first-of-type i:last-of-type'),
+        filterForm = document.querySelector('#p3-filter-runs .p3-filter-runs-form')
 
   filterArrowDown.addEventListener('click', () => {
     filter.classList.toggle('p3-filter-runs-expanded')
     filterArrowDown.classList.toggle('p3-filter-arrow-turn')
+    filterForm.classList.toggle('p3-filter-runs-form-expanded')
   })
 }
 
